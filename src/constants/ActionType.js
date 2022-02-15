df6682dfea5f994083e7313a3d507f43c92eaa03
naveed_hevitt.com <naveed.pieces02@gmail.com>
2022-02-15 22:40:55 +0500

    ready to test

--- src/constants/ActionType.js
+++ src/constants/ActionType.js
@@ -2,4 +2,4 @@ export const Prod = "http://localhost:5000/v1";
 // https://vitalsportal.com/v1
 // http://localhost:5000/v1
 // Hammad's backend
-export const Prod01 = "http://localhost:5000/v1"
+export const Prod01 = "https://vitalsportal.com/v1" 
